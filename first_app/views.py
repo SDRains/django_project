3fd5a29fe45c7dc6515f4b8ab509771a93931611
stephen <applerains1234@gmail.com>
2022-07-30 11:48:33 -0700

    URL mappings done

--- first_app/views.py
+++ first_app/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world!")
+
+
+def home(request):
+    return HttpResponse("Welcome to the home page!")
+
+
+def educative(request):
+    return HttpResponse("Welcome to the educative page!")
